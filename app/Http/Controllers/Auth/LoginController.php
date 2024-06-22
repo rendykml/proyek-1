64439d69b7af546ed87d9d37fb20f2d64ce360af
nesyasal <nesya.sr@gmail.com>
2024-06-22 17:58:30 +0700

    Perbaikan redirect jika pegguna 'Pasien' Login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -29,7 +29,7 @@ class LoginController extends Controller
 					return redirect()->route('admin.dashboard');
 					break;
 				case "Pasien":
-					return redirect()->route('pasien.dashboard');
+					return redirect()->route('pasien.dashboard-keluhan');
 					break;
 				case "Dokter":
 					return redirect()->route('dokter.dashboard');
