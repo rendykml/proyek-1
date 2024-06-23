a12d46314a94ac4daa1074c912f57a22a563a3b8
Valen Rionald <vrionald@gmail.com>
2024-06-23 20:01:20 +0700

    Bantu perbaikan data tidak tampil

--- app/Http/Controllers/KonsultasiController.php
+++ app/Http/Controllers/KonsultasiController.php
@@ -5,12 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use App\Models\Pasien;
 use App\Models\Konsultasi;
 
 class KonsultasiController extends Controller
 {
 	public function dashboard()
 	{
+		// Mendapatkan ID pengguna yang sedang login
+		$userId = Auth::id();
+
+		// Mendapatkan pasien_id dari tabel pasien berdasarkan user_id
+		$pasienId = Pasien::where('user_id', $userId)->value('pasien_id');
+
 		$consultations = Konsultasi::select(
 			'konsultasi_id',
 			'users_pasien.name as nama_pasien',
@@ -21,11 +28,15 @@ class KonsultasiController extends Controller
 			'balasan_dokter'
 		)
 			->join('pasien', 'konsultasi.pasien_id', '=', 'pasien.pasien_id')
-			->join('doctors', 'konsultasi.doctor_id', '=', 'doctors.doctor_id')
+			->join(
+				'doctors',
+				'konsultasi.doctor_id',
+				'=',
+				'doctors.doctor_id'
+			)
 			->join('users as users_pasien', 'pasien.user_id', '=', 'users_pasien.id')
 			->join('users as users_dokter', 'doctors.user_id', '=', 'users_dokter.id')
-			->where('users_pasien.tipe_pengguna', 'Pasien')
-			->where('users_dokter.tipe_pengguna', 'Dokter')
+			->where('pasien.pasien_id', $pasienId)
 			->get();
 
 		return view('pasien.dashboard', compact('consultations'));
@@ -33,10 +44,10 @@ class KonsultasiController extends Controller
 
 	public function dashboardKeluhan()
 	{
-        // Ambil user_id dari user yang sedang login
+		// Ambil user_id dari user yang sedang login
 		$userId = Auth::id();
 
-        // Query untuk ambil data dari tabel pasien dan user untuk mengisi field Nama, Email dan No. Telepon
+		// Query untuk ambil data dari tabel pasien dan user untuk mengisi field Nama, Email dan No. Telepon
 		$pasien = DB::table('pasien')
 			->join('users', 'pasien.user_id', '=', 'users.id')
 			->where('users.tipe_pengguna', 'Pasien')
@@ -44,14 +55,14 @@ class KonsultasiController extends Controller
 			->where('users.id', $userId)
 			->first();
 
-        // Query untuk ambil data dari tabel doctors dan user untuk mengisi field Pilih Dokter
+		// Query untuk ambil data dari tabel doctors dan user untuk mengisi field Pilih Dokter
 		$doctors = DB::table('doctors')
 			->join('users', 'doctors.user_id', '=', 'users.id')
 			->where('users.tipe_pengguna', 'Dokter')
 			->select('users.name as doctor_name', 'users.email', 'users.jenis_kelamin', 'users.alamat', 'users.no_telepon', 'users.tanggal_lahir', 'doctors.doctor_id', 'doctors.spesialisasi', 'doctors.kualifikasi', 'doctors.pengalaman')
 			->get();
 
-        // Menampilkan halaman beserta data yang diambil
+		// Menampilkan halaman beserta data yang diambil
 		return view('pasien.dashboard-keluhan', compact('pasien', 'doctors'));
 	}
 
@@ -67,9 +78,12 @@ class KonsultasiController extends Controller
 		// Mendapatkan user_id dari sesi login
 		$userId = Auth::id();
 
+		// Mendapatkan pasien_id dari tabel pasien berdasarkan user_id
+		$pasienId = DB::table('pasien')->where('user_id', $userId)->value('pasien_id');
+
 		// Insert data ke dalam tabel konsultasi
 		DB::table('konsultasi')->insert([
-			'pasien_id' => $userId,
+			'pasien_id' => $pasienId,
 			'doctor_id' => $request->doctor,
 			'tanggal_konsultasi' => $request->date,
 			'status' => 'belum dijawab',
