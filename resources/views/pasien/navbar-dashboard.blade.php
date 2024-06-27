b66f6d90ab185a32c69b717464d0b8aa08aee4f6
Valen Rionald <vrionald@gmail.com>
2024-06-27 20:16:07 +0700

    Perbaikan button logout di pasien

--- resources/views/pasien/navbar-dashboard.blade.php
+++ resources/views/pasien/navbar-dashboard.blade.php
@@ -12,7 +12,7 @@
 			<ul class="navbar-nav navbar-nav-right">
 				<form method="POST" action="{{ route('logout') }}">
 					@csrf
-					<button type="submit" class="dropdown-item" style="border: none; background: none; padding: 0;"><i class="mdi mdi-logout me-2 text-primary"></i> Signout</button>
+					<button type="submit" class="btn btn-danger"><i class="mdi mdi-logout me-2 text-light"></i> Signout</button>
 				</form>
 			</ul>
 			<button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
